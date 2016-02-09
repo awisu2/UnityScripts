611aa9e058e74ca8dd56d0e8ff803b58e0fc18bf
awisu2 <awisu2@gmail.com>
2016-02-09 10:00:22 +0900

    namespaceの追加

--- Save/Scripts/Saample.cs
+++ Save/Scripts/Saample.cs
@@ -1,5 +1,13 @@
-	private int GetNewVersion() {
-		int version = PlayerPrefs.GetInt ("version", -1);
-		PlayerPrefs.SetInt ("version", ++version);
-		return version;
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace Save {
+	class Sample {
+		private int GetNewVersion() {
+			int version = PlayerPrefs.GetInt ("version", -1);
+			PlayerPrefs.SetInt ("version", ++version);
+			return version;
+		}
 	}
+}

7c04e0e08ea99767568665807bdb0351ff0faa97
awisu2 <awisu2@gmail.com>
2016-02-09 10:14:59 +0900

    Download処理追加

---
+++ Download/Scripts/Sample.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+namespace Download{
+	public class Sample {
+
+		private IEnumerator DownloadImage(string url) {
+			WWW www = new WWW (url);
+
+			//		yield return www;
+			while (!www.isDone) {
+				float progress = www.progress;
+				Debug.Log ((progress * 100f).ToString () + "%");
+				yield return null;
+			}
+
+			if (!string.IsNullOrEmpty (www.error)) {
+				Debug.Log (www.error.ToString ());
+			}
+
+			www.Dispose ();
+		}
+
+		private void Save(WWW www, string name) {
+			byte[] bytes = www.bytes;
+			string path = Application.persistentDataPath + "/" + name;
+			File.WriteAllBytes(path, bytes);
+		}
+	}
+}

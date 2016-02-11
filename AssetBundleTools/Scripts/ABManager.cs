0cbda9b4ffd4e1f0c6430b61b8c7a8c7c19039fc
awisu2 <awisu2@gmail.com>
2016-02-11 23:45:25 +0900

    とりあえずmanifestのロードと１次依存関係の解決

--- AssetBundleTools/Scripts/ABManager.cs
+++ AssetBundleTools/Scripts/ABManager.cs
@@ -8,11 +8,25 @@ namespace AssetBundleTools {
 		static Dictionary<string, AssetBundle> bundles;
 		static Dictionary<string, string> errors;
 
+		static AssetBundleManifest _manifest = null;
+
 		static string _host = "";
 		static string platformName = "";
 
+		static ABManager _manager = null;
+
+		// ホスト
 		public static string host {
 			set{ _host = value; }
+		}
+
+		// マニフェスト
+		public static AssetBundleManifest manifest {
+			set{ _manifest = value; }
+		}
+
+		public static ABManager GetInstance{
+			get{ return _manager; }
 		}
 
 		// 初期化
@@ -35,18 +49,6 @@ namespace AssetBundleTools {
 		{
 		}
 
-		// 現在稼働しているプラットフォームの取得
-		private static RuntimePlatform GetPlatformRunning()
-		{
-			#if UNITY_EDITOR && UNITY_IOS
-			return RuntimePlatform.IPhonePlayer;
-			#elif UNITY_EDITOR && UNITY_ANDROID
-			return RuntimePlatform.Android;
-			#else
-			return Application.platform;
-			#endif
-		}
-
 		// ダウンロードなどの管理のためのGameObject化
 		private static bool InitializeABGameObject(string name)
 		{
@@ -58,6 +60,9 @@ namespace AssetBundleTools {
 			go = new GameObject(name, typeof(ABManager));
 			DontDestroyOnLoad(go);
 
+			// インスタンスの取得
+			_manager = go.GetComponent <ABManager>();
+
 			return true;
 		}
 
@@ -67,25 +72,21 @@ namespace AssetBundleTools {
 		}
 
 		// アセットバンドルのダウンロードと読み込み
-		public static IEnumerator AssetBundleLoad (string assetBundleName, string assetName, int version = 1) {
+		public IEnumerator AssetBundleLoad (string assetBundleName, string assetName, int version = 1, bool isdpend = false) {
 			string url = GetUrl(assetBundleName);
 
 			// ダウンロードを開始して終了を待つ
 			WWW www = WWW.LoadFromCacheOrDownload (url, version);
 			wwws.Add (assetBundleName, www);
 			while (!www.isDone) {
-				#if DEBUG
-				Debug.Log ("[downloading] " + assetBundleName + " " + (www.progress * 100f).ToString () + "%");
-				#endif
+				DebugLog ("[downloading] " + assetBundleName + " " + (www.progress * 100f).ToString () + "%");
 				yield return null;
 			}
 
 			// エラーチェック
 			if(!string.IsNullOrEmpty(www.error)) {
 				errors.Add (assetBundleName, www.error);
-				#if DEBUG
-				Debug.Log ("[download error] " + assetBundleName + " " + www.error);
-				#endif
+				DebugLog ("[download error] " + assetBundleName + " " + www.error);
 				yield break;
 			}
 
@@ -101,6 +102,12 @@ namespace AssetBundleTools {
 			wwws [assetBundleName].Dispose();
 			wwws.Remove (assetBundleName);
 
+		// TODO:依存関係のダウンロードの場合はここで終わり
+			if (isdpend) {
+				AssetBundleLoadDependencies (assetBundleName);
+				yield break;
+			}
+
 			// AssetBundleの中から各種objectを読み込む(非同期)
 			AssetBundleRequest request = bundle.LoadAssetAsync (assetName, typeof(GameObject));
 			yield return request;
@@ -110,8 +117,75 @@ namespace AssetBundleTools {
 			// 不要になったバンドルのクリア
 			bundle.Unload(false);
 
+			// 依存アッセトのロード
+			AssetBundleLoadDependencies (assetBundleName);
+
 			// インスタンス化
 			GameObject.Instantiate (obj);
-		}	
+		}
+
+		// アセットバンドルのダウンロードと読み込み
+		public static IEnumerator ManifestLoad (string manifestName) {
+			string url = GetUrl(manifestName);
+
+			// ダウンロードを開始して終了を待つ
+			WWW www = new WWW(url);
+			wwws.Add (manifestName, www);
+			while (!www.isDone) {
+				DebugLog ("[downloading] " + manifestName + " " + (www.progress * 100f).ToString () + "%");
+				yield return null;
+			}
+
+			// エラーチェック
+			if(!string.IsNullOrEmpty(www.error)) {
+				errors.Add (manifestName, www.error);
+				DebugLog ("[download error] " + manifestName + " " + www.error);
+				yield break;
+			}
+
+			// AssetBundleの抽出
+			AssetBundle bundle = wwws [manifestName].assetBundle;
+			if (bundle == null) {
+				errors.Add (manifestName, wwws[manifestName].error);
+				yield break;
+			}
+
+			// ダウンロード処理のクリア
+			wwws [manifestName].Dispose();
+			wwws.Remove (manifestName);
+
+			// AssetBundleの中から各種objectを読み込む(非同期)
+			AssetBundleRequest request = bundle.LoadAssetAsync ("AssetBundleManifest", typeof(AssetBundleManifest));
+			yield return request;
+
+			_manifest = request.asset as AssetBundleManifest;
+		}
+
+		// 依存アセットバンドルダウンロード
+		public void AssetBundleLoadDependencies(string bundlename) {
+			string[] dependencies = _manifest.GetAllDependencies(bundlename);
+			for (int i = 0; i < dependencies.Length; i++) {
+				Debug.Log (dependencies [i]);
+				StartCoroutine(AssetBundleLoad (dependencies [i], "", 1, true));
+			}
+		}
+
+		// 現在稼働しているプラットフォームの取得
+		private static RuntimePlatform GetPlatformRunning()
+		{
+			#if (UNITY_EDITOR && UNITY_IOS)
+			return RuntimePlatform.IPhonePlayer;
+			#elif (UNITY_EDITOR && UNITY_ANDROID)
+			return RuntimePlatform.Android;
+			#else
+			return Application.platform;
+			#endif
+		}
+
+		private static void DebugLog(string log) {
+			#if DEBUG
+			Debug.Log (log);
+			#endif
+		}
 	}
 }

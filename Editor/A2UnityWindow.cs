8e3d5fb40f66a33085303907463a2fe32fbf5ddb
awisu2 <awisu2@gmail.com>
2016-04-03 00:29:42 +0900

    クラスの分割と整頓

--- Editor/A2UnityWindow.cs
+++ Editor/A2UnityWindow.cs
@@ -24,29 +24,11 @@ namespace org.a2dev.UnityScripts.Editor
         // 行間用の高さ
         const float SPACE_HEIGHT = 10f;
 
-        // orientationの選択Index
-        int orientationIndex = -1;
-
-        public const string DEFAULT_IDENTIFIER = "com.Company.ProductName";
-
-        // オリエンテーション簡単設定
-        string[] OrientationSettings = new string[]
-        {
-            "Default",
-            "Rotation",
-            "Portrait",
-        };
-
         // 初期化フラグ
         bool isInitialize = false;
 
         // playerSettingのトグル表示フラグ
         bool isShowPlaySettings = false;
-
-        // OS毎のdefine設定
-        string defineAlone;
-        string defineAndroid;
-        string defineIos;
 
         Vector2 scrollPosition;
 
@@ -97,7 +79,7 @@ namespace org.a2dev.UnityScripts.Editor
 
             if (isShowPlaySettings)
             {
-                OnGUIStepPlayerSetting();
+                PlayerSettingWindow.OnGUI();
             }
 
             // スクロールビューEnd            
@@ -109,10 +91,6 @@ namespace org.a2dev.UnityScripts.Editor
         /// </summary>
         void Init()
         {
-            // define値の取得
-            defineAlone = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone);
-            defineAndroid = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android);
-            defineIos = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.iOS);
         }
 
         // stepボタン
@@ -184,195 +162,7 @@ namespace org.a2dev.UnityScripts.Editor
             }
         }
 
-        void OnGUIStepPlayerSetting()
-        {
-            GUILayout.BeginHorizontal();
 
-            GUILayout.BeginVertical(GUILayout.Width(5f));
-            GUILayout.Space(1f);
-            GUILayout.EndVertical();
-
-            GUILayout.BeginVertical();
-
-            // bundleIdentifier設定
-            OnGUIStepPlayerSettingBundleIdentifier();
-
-            // orientation設定
-            GUILayout.Label("Orientation");
-            OnGUIStepPlayerSettingOrientation();
-
-            GUILayout.Label("Defines Symbols");
-            OnGUIStepPlayerSettingDefineSymbols();
-            GUILayout.EndVertical();
-
-            GUILayout.EndHorizontal();
-        }
-
-        /// <summary>
-        /// BundleIdentifierGUI
-        /// </summary>
-        void OnGUIStepPlayerSettingBundleIdentifier()
-        {
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("Bundle Identifier", GUILayout.Width(90f));
-
-            string identifier = PlayerSettings.bundleIdentifier;
-            bool isSame = identifier == DEFAULT_IDENTIFIER;
-            GuiStyleUtil.TextField style = null;
-            if (isSame)
-            {
-                style = GuiStyleUtil.TextField.GetInstance().SetTextColor(Color.red);
-            }
-            identifier = GUILayout.TextField(identifier);
-            if (identifier != PlayerSettings.bundleIdentifier)
-            {
-                PlayerSettings.bundleIdentifier = identifier;
-            }
-            if (isSame)
-            {
-                style.Reset();
-            }
-            GUILayout.EndHorizontal();
-        }
-
-        /// <summary>
-        /// オリエンテーションGUI
-        /// </summary>
-        void OnGUIStepPlayerSettingOrientation()
-        {
-            // Default Orientation
-            orientationIndex = OrientationUtil.GetUIOrientationIndex(PlayerSettings.defaultInterfaceOrientation);
-            orientationIndex = EditorGUILayout.Popup(orientationIndex, OrientationUtil.OrientationStrings);
-            PlayerSettings.defaultInterfaceOrientation = OrientationUtil.GetUIOrientation(orientationIndex);
-
-            // Orientationの個別設定(AutoRotationを選択していない時はdisableにする)
-            GUILayout.BeginHorizontal();
-            EditorGUI.BeginDisabledGroup(PlayerSettings.defaultInterfaceOrientation != UIOrientation.AutoRotation);
-            bool isSet = PlayerSettings.allowedAutorotateToLandscapeLeft;
-            if (isSet != GUILayout.Toggle(isSet, "left"))
-            {
-                PlayerSettings.allowedAutorotateToLandscapeLeft = !isSet;
-            }
-            isSet = PlayerSettings.allowedAutorotateToLandscapeRight;
-            if (isSet != GUILayout.Toggle(isSet, "right"))
-            {
-                PlayerSettings.allowedAutorotateToLandscapeRight = !isSet;
-            }
-            isSet = PlayerSettings.allowedAutorotateToPortrait;
-            if (isSet != GUILayout.Toggle(isSet, "up"))
-            {
-                PlayerSettings.allowedAutorotateToPortrait = !isSet;
-            }
-            isSet = PlayerSettings.allowedAutorotateToPortraitUpsideDown;
-            if (isSet != GUILayout.Toggle(isSet, "down"))
-            {
-                PlayerSettings.allowedAutorotateToPortraitUpsideDown = !isSet;
-            }
-            EditorGUI.EndDisabledGroup();
-            GUILayout.EndHorizontal();
-
-            // 設定
-            switch (GUILayout.Toolbar(-1, OrientationSettings))
-            {
-                case 0:
-                    {
-                        PlayerSettings.defaultInterfaceOrientation = UIOrientation.AutoRotation;
-                        PlayerSettings.allowedAutorotateToLandscapeLeft = true;
-                        PlayerSettings.allowedAutorotateToLandscapeRight = true;
-                        PlayerSettings.allowedAutorotateToPortrait = true;
-                        PlayerSettings.allowedAutorotateToPortraitUpsideDown = true;
-                        break;
-                    }
-                case 1:
-                    {
-                        PlayerSettings.defaultInterfaceOrientation = UIOrientation.AutoRotation;
-                        PlayerSettings.allowedAutorotateToLandscapeLeft = true;
-                        PlayerSettings.allowedAutorotateToLandscapeRight = true;
-                        PlayerSettings.allowedAutorotateToPortrait = false;
-                        PlayerSettings.allowedAutorotateToPortraitUpsideDown = false;
-                        break;
-                    }
-                case 2:
-                    {
-                        PlayerSettings.defaultInterfaceOrientation = UIOrientation.AutoRotation;
-                        PlayerSettings.allowedAutorotateToPortrait = true;
-                        PlayerSettings.allowedAutorotateToPortraitUpsideDown = true;
-                        PlayerSettings.allowedAutorotateToLandscapeLeft = false;
-                        PlayerSettings.allowedAutorotateToLandscapeRight = false;
-                        break;
-                    }
-            }
-        }
-
-        /// <summary>
-        /// define設定表示
-        /// </summary>
-        void OnGUIStepPlayerSettingDefineSymbols()
-        {
-            defineAlone = OnGUIDefinesSymbols(BuildTargetGroup.Standalone, "Alone", defineAlone);
-            defineIos = OnGUIDefinesSymbols(BuildTargetGroup.iOS, "iOS", defineIos);
-            defineAndroid = OnGUIDefinesSymbols(BuildTargetGroup.Android, "Android", defineAndroid);
-
-            // 全設定が同時の場合同時に更新が可能
-            bool isSame = (defineAlone == defineIos && defineAlone == defineAndroid);
-            GUILayout.BeginHorizontal();
-            EditorGUI.BeginDisabledGroup(!isSame);
-            GUILayout.Label("All", GUILayout.Width(60f));
-            string define = "-";
-            if (isSame)
-            {
-                define = defineAlone;
-            }
-            string defineInput = GUILayout.TextField(define);
-            if (defineInput != define)
-            {
-                defineAlone = defineInput;
-                defineAndroid = defineInput;
-                defineIos = defineInput;
-            }
-            EditorGUI.BeginDisabledGroup(isSame && define == PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone));
-            if (GUILayout.Button("Set", GUILayout.Width(40f)))
-            {
-                PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, defineInput);
-                PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.iOS, defineInput);
-                PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android, defineInput);
-
-                // フォーマットエラーがあった時用に書き換え  
-                defineInput = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone);
-                defineAlone = defineInput;
-                defineIos = defineInput;
-                defineAndroid = defineInput;
-            }
-            EditorGUI.EndDisabledGroup();
-            EditorGUI.EndDisabledGroup();
-            GUILayout.EndHorizontal();
-
-            // 一括クリアボタン            
-            if (GUILayout.Button("Clear", GUILayout.Width(50f)))
-            {
-                defineAlone = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone);
-                defineAndroid = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android);
-                defineIos = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.iOS);
-            }
-        }
-
-        // 各define値の更新用テキスト表示
-        string OnGUIDefinesSymbols(BuildTargetGroup group, string labelName, string define)
-        {
-            GUILayout.BeginHorizontal();
-            GUILayout.Label(labelName, GUILayout.Width(60f));
-            define = GUILayout.TextField(define);
-            EditorGUI.BeginDisabledGroup(define == PlayerSettings.GetScriptingDefineSymbolsForGroup(group));
-            if (GUILayout.Button("Set", GUILayout.Width(40f)))
-            {
-                PlayerSettings.SetScriptingDefineSymbolsForGroup(group, define);
-                define = PlayerSettings.GetScriptingDefineSymbolsForGroup(group);
-            }
-            EditorGUI.EndDisabledGroup();
-            GUILayout.EndHorizontal();
-
-            return define;
-        }
 
         // シーンリスト
         void OnGUISceanList()
---
+++ Editor/PlayerSettingWindow.cs
@@ -0,0 +1,278 @@
+﻿using UnityEngine;
+using UnityEditor;
+using org.a2dev.UnityScripts.Util;
+
+namespace org.a2dev.UnityScripts.Editor
+{
+    public class PlayerSettingWindow : EditorWindow
+    {
+        // window用の名称(クラス名から)
+        const string NAME_WINDOW = "PlayerSetting";
+
+        // 初期identifier
+        public const string DEFAULT_IDENTIFIER = "com.Company.ProductName";
+
+        // OS毎のdefine設定
+        static string defineAlone;
+        static string defineAndroid;
+        static string defineIos;
+
+        // orientationの選択Index
+        static int orientationIndex = -1;
+
+        // オリエンテーション簡単設定項目
+        static string[] OrientationSettings = new string[]
+        {
+            "Default",
+            "Rotation",
+            "Portrait",
+        };
+
+        static bool isInit = false;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [MenuItem("Window/" + EditorUtil.NAME_EDITOR_PREFIX + "/a2devWindow")]
+        static void OpenWindow()
+        {
+            EditorUtil.GetWindow<PlayerSettingWindow>(NAME_WINDOW).Show();
+        }
+
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        public static void Init()
+        {
+            // define値の取得
+            defineAlone = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone);
+            defineAndroid = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android);
+            defineIos = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.iOS);
+        }
+
+        /// <summary>
+        /// 一度のみの初期化
+        /// </summary>
+        public static void InitOnce()
+        {
+            if (isInit == false)
+            {
+                Init();
+                isInit = true;
+            }
+        }
+
+        /// <summary>
+        /// OnGUI
+        /// </summary>
+        public static void OnGUI()
+        {
+            InitOnce();
+
+            GUILayout.BeginHorizontal();
+
+            GUILayout.BeginVertical(GUILayout.Width(5f));
+            GUILayout.Space(1f);
+            GUILayout.EndVertical();
+
+            GUILayout.BeginVertical();
+
+            // bundleIdentifier設定
+            OnGUIStepPlayerSettingBundleIdentifier();
+
+            // orientation設定
+            GUILayout.Label("Orientation");
+            OnGUIStepPlayerSettingOrientation();
+
+            GUILayout.Label("Defines Symbols");
+            OnGUIStepPlayerSettingDefineSymbols();
+            GUILayout.EndVertical();
+
+            GUILayout.EndHorizontal();
+        }
+
+        /// <summary>
+        /// BundleIdentifierGUI
+        /// </summary>
+        public static void OnGUIStepPlayerSettingBundleIdentifier()
+        {
+            InitOnce();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Bundle Identifier", GUILayout.Width(90f));
+
+            string identifier = PlayerSettings.bundleIdentifier;
+            bool isSame = identifier == DEFAULT_IDENTIFIER;
+            GuiStyleUtil.TextField style = null;
+            if (isSame)
+            {
+                style = GuiStyleUtil.TextField.GetInstance().SetTextColor(Color.red);
+            }
+            identifier = GUILayout.TextField(identifier);
+            if (identifier != PlayerSettings.bundleIdentifier)
+            {
+                PlayerSettings.bundleIdentifier = identifier;
+            }
+            if (isSame)
+            {
+                style.Reset();
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        /// <summary>
+        /// オリエンテーションGUI
+        /// </summary>
+        public static void OnGUIStepPlayerSettingOrientation()
+        {
+            InitOnce();
+
+            // Default Orientation
+            orientationIndex = OrientationUtil.GetUIOrientationIndex(PlayerSettings.defaultInterfaceOrientation);
+            orientationIndex = EditorGUILayout.Popup(orientationIndex, OrientationUtil.OrientationStrings);
+            PlayerSettings.defaultInterfaceOrientation = OrientationUtil.GetUIOrientation(orientationIndex);
+
+            // Orientationの個別設定(AutoRotationを選択していない時はdisableにする)
+            GUILayout.BeginHorizontal();
+            EditorGUI.BeginDisabledGroup(PlayerSettings.defaultInterfaceOrientation != UIOrientation.AutoRotation);
+            bool isSet = PlayerSettings.allowedAutorotateToLandscapeLeft;
+            if (isSet != GUILayout.Toggle(isSet, "left"))
+            {
+                PlayerSettings.allowedAutorotateToLandscapeLeft = !isSet;
+            }
+            isSet = PlayerSettings.allowedAutorotateToLandscapeRight;
+            if (isSet != GUILayout.Toggle(isSet, "right"))
+            {
+                PlayerSettings.allowedAutorotateToLandscapeRight = !isSet;
+            }
+            isSet = PlayerSettings.allowedAutorotateToPortrait;
+            if (isSet != GUILayout.Toggle(isSet, "up"))
+            {
+                PlayerSettings.allowedAutorotateToPortrait = !isSet;
+            }
+            isSet = PlayerSettings.allowedAutorotateToPortraitUpsideDown;
+            if (isSet != GUILayout.Toggle(isSet, "down"))
+            {
+                PlayerSettings.allowedAutorotateToPortraitUpsideDown = !isSet;
+            }
+            EditorGUI.EndDisabledGroup();
+            GUILayout.EndHorizontal();
+
+            // 設定
+            SetOrientationCustom(GUILayout.Toolbar(-1, OrientationSettings));
+        }
+
+        /// <summary>
+        /// Orientationの設定を行う
+        /// </summary>
+        /// <param name="index">カスタム設定index</param>
+        static void SetOrientationCustom(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    {
+                        PlayerSettings.defaultInterfaceOrientation = UIOrientation.AutoRotation;
+                        PlayerSettings.allowedAutorotateToLandscapeLeft = true;
+                        PlayerSettings.allowedAutorotateToLandscapeRight = true;
+                        PlayerSettings.allowedAutorotateToPortrait = true;
+                        PlayerSettings.allowedAutorotateToPortraitUpsideDown = true;
+                        break;
+                    }
+                case 1:
+                    {
+                        PlayerSettings.defaultInterfaceOrientation = UIOrientation.AutoRotation;
+                        PlayerSettings.allowedAutorotateToLandscapeLeft = true;
+                        PlayerSettings.allowedAutorotateToLandscapeRight = true;
+                        PlayerSettings.allowedAutorotateToPortrait = false;
+                        PlayerSettings.allowedAutorotateToPortraitUpsideDown = false;
+                        break;
+                    }
+                case 2:
+                    {
+                        PlayerSettings.defaultInterfaceOrientation = UIOrientation.AutoRotation;
+                        PlayerSettings.allowedAutorotateToPortrait = true;
+                        PlayerSettings.allowedAutorotateToPortraitUpsideDown = true;
+                        PlayerSettings.allowedAutorotateToLandscapeLeft = false;
+                        PlayerSettings.allowedAutorotateToLandscapeRight = false;
+                        break;
+                    }
+            }
+        }
+
+        /// <summary>
+        /// define設定表示
+        /// </summary>
+        public static void OnGUIStepPlayerSettingDefineSymbols()
+        {
+            InitOnce();
+
+            defineAlone = OnGUIDefinesSymbols(BuildTargetGroup.Standalone, "Alone", defineAlone);
+            defineIos = OnGUIDefinesSymbols(BuildTargetGroup.iOS, "iOS", defineIos);
+            defineAndroid = OnGUIDefinesSymbols(BuildTargetGroup.Android, "Android", defineAndroid);
+
+            // 全設定が同時の場合同時に更新が可能
+            bool isSame = (defineAlone == defineIos && defineAlone == defineAndroid);
+            GUILayout.BeginHorizontal();
+            EditorGUI.BeginDisabledGroup(!isSame);
+            GUILayout.Label("All", GUILayout.Width(60f));
+            string define = "-";
+            if (isSame)
+            {
+                define = defineAlone;
+            }
+            string defineInput = GUILayout.TextField(define);
+            if (defineInput != define)
+            {
+                defineAlone = defineInput;
+                defineAndroid = defineInput;
+                defineIos = defineInput;
+            }
+            EditorGUI.BeginDisabledGroup(isSame && define == PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone));
+            if (GUILayout.Button("Set", GUILayout.Width(40f)))
+            {
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, defineInput);
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.iOS, defineInput);
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android, defineInput);
+
+                // フォーマットエラーがあった時用に書き換え  
+                defineInput = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone);
+                defineAlone = defineInput;
+                defineIos = defineInput;
+                defineAndroid = defineInput;
+            }
+            EditorGUI.EndDisabledGroup();
+            EditorGUI.EndDisabledGroup();
+            GUILayout.EndHorizontal();
+
+            // 一括クリアボタン            
+            if (GUILayout.Button("Clear", GUILayout.Width(50f)))
+            {
+                defineAlone = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone);
+                defineAndroid = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android);
+                defineIos = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.iOS);
+            }
+        }
+
+        // 各define値の更新用テキスト表示
+        static string OnGUIDefinesSymbols(BuildTargetGroup group, string labelName, string define)
+        {
+            InitOnce();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(labelName, GUILayout.Width(60f));
+            define = GUILayout.TextField(define);
+            EditorGUI.BeginDisabledGroup(define == PlayerSettings.GetScriptingDefineSymbolsForGroup(group));
+            if (GUILayout.Button("Set", GUILayout.Width(40f)))
+            {
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(group, define);
+                define = PlayerSettings.GetScriptingDefineSymbolsForGroup(group);
+            }
+            EditorGUI.EndDisabledGroup();
+            GUILayout.EndHorizontal();
+
+            return define;
+        }
+    }
+
+}
